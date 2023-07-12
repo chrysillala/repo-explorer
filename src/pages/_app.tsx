56c404e2808e3f408e346be8c7595c03c7ed3a17
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2023-07-12 14:31:56 +0700

    update app to include react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from "react";
+import "@/styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import type { AppProps } from "next/app";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}></Hydrate>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
 }
