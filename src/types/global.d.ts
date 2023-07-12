eb18953215984f2829a2c45a57b57cad91ae32ca
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2023-07-12 14:24:47 +0700

    add global types

---
+++ src/types/global.d.ts
@@ -0,0 +1,23 @@
+import { Endpoints } from "@octokit/types";
+
+export {};
+
+declare global {
+  type FormQueryHook = {
+    query: string;
+    isLoading: boolean;
+    handleSubmit: (...args: any[]) => void;
+    handleLoading: (...args: any[]) => void;
+  };
+
+  type PaginationHook = {
+    currentPage: number;
+    handlePageChange: (...args: any[]) => void;
+  };
+
+  type SearchUsersResponse = Endpoints["GET /search/users"]["response"];
+  type SearchUsersItemResponse =
+    Endpoints["GET /search/users"]["response"]["data"]["items"];
+  type ListUserReposResponse =
+    Endpoints["GET /users/{username}/repos"]["response"];
+}
