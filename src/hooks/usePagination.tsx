dcbacb36de401a8aea9f7bc1ea2f33fa5cbd1f64
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2023-08-03 16:17:33 +0700

    add comment

--- src/hooks/usePagination.tsx
+++ src/hooks/usePagination.tsx
@@ -41,7 +41,7 @@ const PaginationProvider: FC<Props> = ({ children }) => {
 const usePagination = () => {
   const context = useContext(PaginationContext);
   if (context === undefined) {
-    throw new Error("useGlobal must be used within a PaginationProvider");
+    throw new Error("usePagination must be used within a PaginationProvider");
   }
   return context as PaginationHook;
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,10 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PaginationProvider } from "@/hooks/usePagination";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // https://tanstack.com/query/v4/docs/react/guides/ssr#using-hydration
+  // Create the QueryClient in React state to support caching queries on the server and set up hydration
+  // This ensures that data is not shared between different users and requests,
+  // while still only creating the QueryClient once per component lifecycle.
   const [queryClient] = useState(() => new QueryClient());
 
   return (
